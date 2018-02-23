01e65910170f68d896426369f33f3715c36e4951
Chris Casey <cdcasey@gmail.com>
2018-02-23 15:45:28 -0600

    Edit event set up with styles and input field. TODO enter when done editing.

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 let addButton = document.querySelector('#add_todo');
-let inputField = document.querySelector('#inputField');
+let inputField = document.querySelector('#input-field');
 let todoItems = document.querySelector('#ul--todo-items');
 
+let editField = document.createElement("input");
+editField.setAttribute('class', 'input-edit-field');
+let itemString = '<input class="check-off" type="checkbox"><span class="todo-text">%</span><span class="delete-item">X</span>';
+
 addButton.addEventListener('click', () => {
-    console.log(inputField.value);
-    let newItem = document.createElement('li');
-    newItem.innerText = inputField.value;
-    todoItems.appendChild(newItem);
+    if (inputField.value) {
+        let newItem = document.createElement('li');
+        newItem.innerHTML = itemString.replace('%', inputField.value);
+        todoItems.appendChild(newItem);
+        inputField.value = "";
+    }
+})
+
+todoItems.addEventListener('click', (event) => {
+    // console.log(event.target.parentNode);
+    if (event.target.classList.contains("check-off")) {
+        event.target.parentNode.setAttribute('class', 'completed');
+    } else if (event.target.classList.contains('delete-item')) {
+        event.target.parentNode.remove();
+    } else if (event.target.classList.contains('todo-text')) {
+        let todoText = event.target.innerText;
+        // TODO: remove text. insert edit vield with removed text. handle hitting the enter key
+        event.target.parentNode.appendChild(editField);
+        editField.value = todoText;
+        event.target.remove();
+    }
 
 })
